38658c58fde55757fce2c599b1605763bd17da30
phirmware <chibuzor.ojukwu@gmail.com>
2018-10-18 10:51:14 +0000

    notify

--- routes/notification.js
+++ routes/notification.js
@@ -2,20 +2,31 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.post('/new',(req,res)=>{
-    db.notifications.create(req.body).then(notification=>{
-        res.json({statusText:'Successfully sent',statusCode:200});
-    }).catch(err=>{
+router.post('/new', (req, res) => {
+    db.notifications.create(req.body).then(notification => {
+        res.json({ statusText: 'Successfully sent', statusCode: 200 });
+    }).catch(err => {
         res.send(err);
     });
 });
 
-router.get('/:id',(req,res)=>{
-    db.notifications.find({userId:req.params.id}).then(notifications=>{
-        res.json(notifications);
-    }).catch(err=>{
+
+router.get('/general', (req, res) => {
+    db.notifications.find().then(nots => {
+        res.json(nots);
+    }).catch(err => {
         res.send(err);
     });
 });
+
+
+router.get('/:id', (req, res) => {
+    db.notifications.find({ userId: req.params.id }).then(notifications => {
+        res.json(notifications);
+    }).catch(err => {
+        res.send(err);
+    });
+});
+
 
 module.exports = router;
