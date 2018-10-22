82da20f03d25a521ed26e19accae221978162dd6
phirmware <chibuzor.ojukwu@gmail.com>
2018-10-22 18:00:17 +0000

    ok change

--- models/notification.js
+++ models/notification.js
@@ -1,32 +1,28 @@
 var mongoose = require('mongoose');
-var express =  require('express');
+var express = require('express');
 
 var notificationSchema = new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true
+    userId: {
+        type: String,
+        required: true
     },
-   location:{
-       type:String,
-       required:true
-   },
-   details:{
-       type:String,
-       required:true
-   },
-   price:{
-       type:String,
-       required:true
-   },
-   email:{
-       type:String,
-       required:true
-   },
-   phone:{
-       type:String,
-       required:true
-   }
+    pro: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    time_sent:{
+        type:Date,
+        default:Date.now
+    }
 });
 
-var Notifications = mongoose.model('notification',notificationSchema);
+var Notifications = mongoose.model('notification', notificationSchema);
 module.exports = Notifications;
